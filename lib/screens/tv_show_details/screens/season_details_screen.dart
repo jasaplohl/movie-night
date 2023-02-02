95dd79dbc6dc26dfee91bbdb7200517a292d5b09
jasa-plohl <jasa.plohl@devleet.si>
2023-02-02 08:41:03 +0100

    Paginated episodes.

--- lib/screens/tv_show_details/screens/season_details_screen.dart
+++ lib/screens/tv_show_details/screens/season_details_screen.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:movie_night/models/episode_model.dart';
 import 'package:movie_night/models/season_model.dart';
 import 'package:movie_night/services/common_services.dart';
+import 'package:movie_night/services/constants.dart';
 import 'package:movie_night/services/show_error_dialog.dart';
 import 'package:movie_night/services/tv_show_service.dart';
 import 'package:movie_night/widgets/divider_margin.dart';
 import 'package:movie_night/widgets/loading_spinner.dart';
+import 'package:movie_night/widgets/pagination.dart';
 import 'package:movie_night/widgets/rating_chip.dart';
 
 class SeasonDetailsScreen extends StatefulWidget {
@@ -21,16 +23,41 @@ class _SeasonDetailsScreenState extends State<SeasonDetailsScreen> {
 
   List<Episode>? episodes;
 
+  // Pagination
+  int currentPage = 1;
+  int totalPages = 1;
+  List<Episode>? displayedEpisodes;
+
+  ScrollController scrollController = ScrollController();
+
   @override
   void initState() {
     getEpisodes(widget.tvShowId, widget.season.seasonNumber).then((List<Episode> value) {
       setState(() {
+        totalPages = (value.length / maxItemsPerPage).ceil();
         episodes = value;
       });
+      setEpisodesForCurrentPage();
     }).catchError((err) {
       showErrorDialog(context, err.toString());
     });
     super.initState();
+  }
+
+  void onPageChange(int newPage) {
+    setState(() {
+      currentPage = newPage;
+    });
+    scrollController.animateTo(0, duration: const Duration(milliseconds: 500), curve: Curves.easeOut);
+    setEpisodesForCurrentPage();
+  }
+
+  void setEpisodesForCurrentPage() {
+    final lowerLimit = (currentPage - 1) * maxItemsPerPage;
+    final upperLimit = currentPage * maxItemsPerPage > episodes!.length ? episodes!.length : currentPage * maxItemsPerPage;
+    setState(() {
+      displayedEpisodes = episodes!.sublist(lowerLimit, upperLimit);
+    });
   }
 
   @override
@@ -40,6 +67,7 @@ class _SeasonDetailsScreenState extends State<SeasonDetailsScreen> {
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
         child: ListView(
+          controller: scrollController,
           children: [
             Text(widget.season.name, style: Theme.of(context).textTheme.headlineLarge),
             if(widget.season.airDate != null) Text("First air date: ${formatDate(DateTime.parse(widget.season.airDate!))}"),
@@ -58,10 +86,10 @@ class _SeasonDetailsScreenState extends State<SeasonDetailsScreen> {
             ),
             const DividerMargin(),
             Text("Episodes", style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Theme.of(context).primaryColorLight)),
-            // TODO: paginate the episodes list if there are more than 20 episodes
-            episodes == null ? const LoadingSpinner() : Column(
+            displayedEpisodes == null ? const LoadingSpinner() : Column(
               children: [
-                for(final Episode episode in episodes!) ListTile(
+                Text("Current page: $currentPage of $totalPages."),
+                for(final Episode episode in displayedEpisodes!) ListTile(
                   leading: episode.stillPath != null ? FadeInImage.assetNetwork(
                     image: getBackdropUrl(episode.stillPath!),
                     placeholder: "lib/assets/images/default_img.webp",
@@ -78,6 +106,11 @@ class _SeasonDetailsScreenState extends State<SeasonDetailsScreen> {
                   subtitle: Text(episode.name),
                   trailing: RatingChip(rating: episode.voteAverage),
                   dense: false,
+                ),
+                Pagination(
+                    currentPage: currentPage,
+                    totalPages: totalPages,
+                    onPageChange: onPageChange
                 ),
               ],
             ),
